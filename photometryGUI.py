3aa6bb0de2a8ff9cb98d26fe235985e672df8e59
Jaime McCutcheon <jem64@le.ac.uk>
2018-02-27 07:29:35 +0000

    Made first couple of buttons
    And added a layout.xls file

--- photometryGUI.py
+++ photometryGUI.py
@@ -28,20 +28,46 @@ class Window(Frame):
     def __init__(self, master=None):
         f1 = ttk.Style()
         f1.configure('.', background='powder blue', padding=5)
+        f1.configure('TButton', width=15, sticky=(E,W))
+        f2 = ttk.Style()
+        f2.configure('inner.TFrame', background='light cyan')
         
         ttk.Frame.__init__(self, master, style='TFrame', padding=(10, 10, 15, 15))               
-        self.master = master
+
+
         
+        self.master = master        
         self.init_window()
 
     def init_window(self):
         self.master.title('Photometry Analyzer')
         self.pack(fill=BOTH, expand=1)
+        
+        #Frame for session window
+#        self.f2 = ttk.Frame(self, style='inner.TFrame',# borderwidth=5,
+#                            relief="sunken", width=300, height=500)
+#        
+#        self.f2.grid(self, column=0, row=2, columnspan=6, rowspan=3, sticky=(N,S,E,W))
+
+        self.converttdtBtn = ttk.Button(self, text='Convert TDT File', command=self.converttdt)
+        self.loadfileBtn = ttk.Button(self, text='Load File', command=self.loadfile)
+    
+        self.converttdtBtn.grid(column=0, row=0)
+        self.loadfileBtn.grid(column=0, row=1)
+        
+    def converttdt(self):
+        alert('Feature coming soon!')
+    def loadfile(self):
+        alert('Feature coming soon!')
+        
+        
+def alert(msg):
+    print(msg)
+    messagebox.showinfo('Error', msg)
 
 root = Tk()
 
 currdir = os.getcwd()
-currdir = 'C:\\Users\\jaimeHP\\Dropbox\\Python\\cas9\\cas9_medfiles\\'
 
 app = Window(root)
 root.lift()
