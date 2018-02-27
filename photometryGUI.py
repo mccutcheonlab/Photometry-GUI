66a82a402921da00ac940b03cfe8f23b7106423c
Jaime McCutcheon <jem64@le.ac.uk>
2018-02-27 13:59:02 +0000

    Added more buttons

--- photometryGUI.py
+++ photometryGUI.py
@@ -52,8 +52,42 @@ class Window(Frame):
         self.converttdtBtn = ttk.Button(self, text='Convert TDT File', command=self.converttdt)
         self.loadfileBtn = ttk.Button(self, text='Load File', command=self.loadfile)
     
+        self.shortfilename = StringVar(self.master)
+        self.shortfilename.set('No file loaded')
+        self.filenameLbl = ttk.Label(self, textvariable=self.shortfilename)
+        
+        self.baselineLbl = ttk.Label(self, text='Baseline (s)')
+        self.snipitLbl = ttk.Label(self, text='Snipit length (s)')
+        self.nbinsLbl = ttk.Label(self, text='No. of bins')
+        
+        self.baseline = StringVar(self.master)
+        self.baselineField = ttk.Entry(self, textvariable=self.baseline)
+        self.baselineField.insert(END, '10')
+
+        self.snipitlength = StringVar(self.master)
+        self.snipitField = ttk.Entry(self, textvariable=self.snipitlength)
+        self.snipitField.insert(END, '30')
+        
+        self.nbins = StringVar(self.master)
+        self.nbinsField = ttk.Entry(self, textvariable=self.nbins)
+        self.nbinsField.insert(END, '300')
+       
+        self.aboutLbl = ttk.Label(self, text='Photometry Analyzer-1.0 by J McCutcheon')
+
+    
+    
         self.converttdtBtn.grid(column=0, row=0)
         self.loadfileBtn.grid(column=0, row=1)
+        self.filenameLbl.grid(column=0, row=2, sticky=W)
+        
+        self.baselineLbl.grid(column=0, row=3, sticky=E)
+        self.baselineField.grid(column=1, row=3)
+        self.snipitLbl.grid(column=2, row=3, sticky=E)
+        self.snipitField.grid(column=3, row=3)
+        self.nbinsLbl.grid(column=4, row=3, sticky=E)
+        self.nbinsField.grid(column=5, row=3)
+        
+        self.aboutLbl.grid(column=0, row=5, columnspan=3, sticky=W)
         
     def converttdt(self):
         alert('Feature coming soon!')
