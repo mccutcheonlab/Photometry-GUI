3032d3861c7a993aae43edea1a24b5bea75267ec
Jaime McCutcheon <mccutcheon.james@gmail.com>
2019-09-23 08:43:32 +0200

    got process signals and make snips working

--- photometryGUI.py
+++ photometryGUI.py
@@ -23,6 +23,7 @@ import ntpath
 import csv
 import collections
 import tdt
+import scipy.signal as sig
 
 #import JM_general_functions as jmf
 #import JM_custom_figs as jmfig
@@ -57,14 +58,14 @@ class Window(Frame):
         self.f4 = ttk.Frame(self, style='inner.TFrame', relief='sunken',
                             borderwidth=5, height=200, width=400)
 
-        self.converttdtBtn = ttk.Button(self, text='Convert TDT File', command=self.converttdt)
-        self.loadfileBtn = ttk.Button(self, text='Load File', command=self.loadfile)
+        self.processBtn = ttk.Button(self, text='Process Signals', command=self.processdata)
+        self.choosefileBtn = ttk.Button(self, text='Choose File', command=self.choosefile)
         self.makesnipsBtn = ttk.Button(self, text='Make Snips', command=self.makesnips)
         self.makelickrunsBtn = ttk.Button(self, text='Lick runs', command=self.makelickruns)
-        self.viewsessionBtn = ttk.Button(self, text='View Session', command=self.sessionviewer)
+        self.loaddataBtn = ttk.Button(self, text='Load data', command=self.loaddata)
         
         self.shortfilename = StringVar(self.master)
-        self.shortfilename.set('No file loaded')
+        self.shortfilename.set('No file chosen')
         self.filenameLbl = ttk.Label(self, textvariable=self.shortfilename)
         
         self.baselineLbl = ttk.Label(self, text='Baseline (s)')
@@ -86,18 +87,18 @@ class Window(Frame):
 
         self.progress = ttk.Progressbar(self, orient=HORIZONTAL, length=200, mode='determinate')
 
-        self.aboutLbl = ttk.Label(self, text='Photometry Analyzer-1.1 by J McCutcheon')
+        self.aboutLbl = ttk.Label(self, text='Photometry Analyzer-2.0 by J McCutcheon')
 # Packing grid with widgets
         
         self.f2.grid(column=2, row=0, columnspan=8, rowspan=3, sticky=(N,S,E,W))
         self.f3.grid(column=0, row=4, columnspan=4, sticky=(N,S,E,W))
         self.f4.grid(column=4, row=4, columnspan=4, sticky=(N,S,E,W))
         
-        self.converttdtBtn.grid(column=0, row=0)
-        self.loadfileBtn.grid(column=0, row=1)
+        self.processBtn.grid(column=0, row=0)
+        self.choosefileBtn.grid(column=0, row=1)
         self.filenameLbl.grid(column=0, row=2, columnspan=2, sticky=W)
         
-        self.viewsessionBtn.grid(column=1, row=0)
+        self.loaddataBtn.grid(column=1, row=0)
         
         self.baselineLbl.grid(column=0, row=3, sticky=E)
         self.baselineField.grid(column=1, row=3)
@@ -122,86 +123,37 @@ class Window(Frame):
         
         self.sessionviewer()
         
-    def converttdt(self):
-        alert('Feature coming soon!')
+    def choosefile(self):
+        # open window to choose file
+        #self.tdtfile = filedialog.askopenfilename(initialdir=currdir, title='Select a file.')
+        self.tdtfile = 'C:\\Github\\PPP_analysis\\data\\Eelke-171027-111329\\'
+        self.shortfilename.set(ntpath.basename(self.tdtfile))
         
-    def loadfile(self):
-        #self.filename = filedialog.askopenfilename(initialdir=currdir, title='Select a file.')
-        self.filename = 'C:\\Github\\PPP_analysis\\data\\Eelke-171027-111329\\'
-        self.shortfilename.set(ntpath.basename(self.filename))
-        self.opentdtfile()
-    
-    def opentdtfile(self):
-        tmp = tdt.read_block(self.filename, t2=2, evtype=['streams'])
-        self.streamfields = [v for v in vars(tmp.streams) if v not 'Fi2r']
-        tmp = tdt.read_block(self.filename, evtype=['epocs'])
-        self.epochfields = [v for v in vars(tmp.epocs)]
-        print('Read in file')
+        # opens file to get stream and epoch names
+        self.getstreamandepochnames()
         
-#        a = sio.loadmat(self.filename, squeeze_me=True, struct_as_record=False) 
-#        self.output = a['output']
-#        try:
-#            self.fs = self.output.fs
-#        except:
-#            self.fs = self.output.fs1     # add line to search for fs variable in output
-#        self.getstreamfields()
-#        self.getepochfields()
+        # set tick
+        self.setticks()
+        
+        # update dropdown menu options
         self.updatesigoptions()
-       
-    def makesnips(self):
-        self.setevents()
-        self.bins = int(self.nbins.get())
-        self.time2samples()
-        self.randomevents = makerandomevents(120, max(self.tick)-120)
-        self.bgTrials, self.pps = jmf.snipper(self.data, self.randomevents,
-                                        t2sMap = self.t2sMap, fs = self.fs, bins=self.bins)
-        self.snips = jmf.mastersnipper(self, self.events)
-        self.getnoiseindex()
-        self.singletrialviewer()
-        self.averagesnipsviewer()
-    
-    def getnoiseindex(self):
-        self.noisemethod = 'sum'
-        self.threshold = 8
-        bgMAD = jmf.findnoise(self.data, self.randomevents,
-                              t2sMap = self.t2sMap, fs = self.fs, bins=self.bins,
-                              method=self.noisemethod)          
-        sigSum = [np.sum(abs(i)) for i in self.snips['blue']]
-        sigSD = [np.std(i) for i in self.snips['blue']]
-        self.noiseindex = [i > bgMAD*self.threshold for i in sigSum]
-
-    def makelickruns(self):
-        self.setevents()
-        # need to set lick runs as if a normal output variable
-        self.runs = [val for i, val in enumerate(self.licks) if (val - self.licks[i-1] > 10)]
-        self.epochfields.append('runs')
         self.updateeventoptions()
-
-    def getstreamfields(self):
-        self.streamfields = [v for v in vars(self.tmp.streams)]
-        print(self.streamfields)
-#        for x in vars(self.tmp.streams):
-#            try:
-#                len(getattr(self.output, x))
-#                self.streamfields.append(x)
-#            except: pass
-            
-    def getepochfields(self):
-        self.epochfields = []
-        for x in self.output._fieldnames:
-            var = getattr(self.output, x)
-            if hasattr(var, 'onset'):
-                self.epochfields.append(x)
+    
+    def getstreamandepochnames(self):
+        tmp = tdt.read_block(self.tdtfile, t2=2, evtype=['streams'])
+        self.streamfields = [v for v in vars(tmp.streams) if v != 'Fi2r']
+        
+        tmp = tdt.read_block(self.tdtfile, evtype=['epocs'])
+        self.epocs = getattr(tmp, 'epocs')
+        self.epochfields = [v for v in vars(self.epocs)]
+        
+    def setticks(self):
         try:
-            self.tick = self.output.tick.onset
+            self.tick = self.epocs.Tick['onset']
+            print('Ticks set.')
             return
-        except: pass
-        try:
-            self.tick = self.output.Tick.onset
-            return
-        except:
-            self.tick = self.output.tick
-
+        except: print('Could not assign tick automatically')
+        
     def updatesigoptions(self):
         try:
             sigOptions = self.streamfields
@@ -214,12 +166,6 @@ class Window(Frame):
         self.chooseblueMenu.grid(column=1, row=1)
         self.chooseuvMenu.grid(column=1, row=2)
 
-    def setsignals(self):       
-        try:
-            self.data = getattr(self.output, self.blue.get())
-            self.dataUV = getattr(self.output, self.uv.get())
-        except AttributeError: pass
-    
     def updateeventoptions(self):
         try:
             eventOptions = self.epochfields
@@ -237,58 +183,129 @@ class Window(Frame):
 
         self.chooselicksMenu = ttk.OptionMenu(self, self.licksVar, *lickOptions)
         self.chooselicksMenu.grid(column=8, row=3)
+        
+    def loaddata(self):
+        # load in streams
+        self.loadstreams()
+        
+        # process data
+        self.processdata()
+        
+        # plot all session data
+        self.sessionviewer()
+        
+    def loadstreams(self):       
+        try:
+            tmp = tdt.read_block(self.tdtfile, evtype=['streams'], store=self.blue.get())
+            self.data = getattr(tmp.streams, self.blue.get())['data']
+            self.fs = getattr(tmp.streams, self.blue.get())['fs']
+            
+            tmp = tdt.read_block(self.tdtfile, evtype=['streams'], store=self.uv.get())
+            self.datauv = getattr(tmp.streams, self.uv.get())['data']
+        except:
+            print('No file chosen yet or problem extracting signals')
+    
+    def processdata(self):
+        pt = len(self.data)
+        X = np.fft.rfft(self.datauv, pt)
+        Y = np.fft.rfft(self.data, pt)
+        Ynet = Y-X
+    
+        datafilt = np.fft.irfft(Ynet)
+    
+        datafilt = sig.detrend(datafilt)
+    
+        b, a = sig.butter(9, 0.012, 'low', analog=True)
+        self.datafilt = sig.filtfilt(b, a, datafilt)
+        
+    def sessionviewer(self):
+        f = Figure(figsize=(7,2))
+        ax = f.subplots(ncols=2)
 
+        try:
+            ax[0].plot(self.data, color='blue')
+        except AttributeError: pass
+        try:
+            ax[0].plot(self.datauv, color='m')
+        except AttributeError: pass
+    
+        try:
+            ax[1].plot(self.datafilt, color='g')
+        except: pass
+        
+        for axis in ax:
+            try:
+                axis.set_xticks(np.multiply([0, 10, 20, 30, 40, 50, 60],60*self.fs))
+                axis.set_xticklabels(['0', '10', '20', '30', '40', '50', '60'])
+                axis.set_xlabel('Time (min)')        
+            except: pass
+#        try:
+#            combined = np.concatenate((self.data, self.datauv), axis=0)
+#            upper = jmf.findpercentilevalue(combined, 0.95)
+#            lower = jmf.findpercentilevalue(combined, 0.05)
+#            ax[1].set_ylim([lower, upper])
+#        except: print('Getting y-axis limits for session viewer is not working')
+        
+#        try:
+#            for i, x in enumerate(self.epochfields):
+#                print(i, x)
+##                onset = getattr(output, x).onset * self.fs
+##                ax[0].scatter(onset, [i]*len(onset))
+#        except:
+#            print('could not plot events')
+#            pass
+        
+        canvas = FigureCanvasTkAgg(f, self.f2)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=0, column=0, sticky=(N,S,E,W))
+
+    def makesnips(self):
+        # get events and number of bins from dropdown menus
+        self.setevents()
+        self.bins = int(self.nbins.get())
+        
+        # extract snips and calculate noise from data
+        self.time2samples()
+        self.randomevents = makerandomevents(120, max(self.tick)-120)
+        self.bgTrials, self.pps = snipper(self.data, self.randomevents,
+                                        t2sMap = self.t2sMap, fs = self.fs, bins=self.bins)
+        self.snips = mastersnipper(self, self.events)
+        self.getnoiseindex()
+        
+        # plot data
+        self.singletrialviewer()
+        self.averagesnipsviewer()
+        
     def setevents(self):
         try:
             if self.eventsVar.get() == 'runs':
                 self.events = self.runs
             else:
-                self.event = getattr(self.output, self.eventsVar.get())
-                self.events = getattr(self.event, self.onsetVar.get())
+                self.eventepoc = getattr(self.epocs, self.eventsVar.get())
+                self.events = getattr(self.eventepoc, self.onsetVar.get())
 
-            self.lick = getattr(self.output, self.licksVar.get())
-            self.licks = getattr(self.lick, self.onsetVar.get())
+            self.lickepoc = getattr(self.epocs, self.licksVar.get())
+            self.licks = getattr(self.lickepoc, self.onsetVar.get())
         except:
             alert('Cannot set event')
-        
-    def sessionviewer(self):
-        self.updateeventoptions()
-        self.setsignals()
 
-#        f = Figure(figsize=(7,2))
-        f, ax = plt.subplots(figsize=(7,2), nrows=2, sharex=True)
-#        ax = f.add_subplot(111)
-        try:
-            ax[1].plot(self.data, color='blue')
-        except AttributeError: pass
-        try:
-            ax[1].plot(self.dataUV, color='m')
-        except AttributeError: pass
-        try:
-            ax[1].set_xticks(np.multiply([0, 10, 20, 30, 40, 50, 60],60*self.fs))
-            ax[1].set_xticklabels(['0', '10', '20', '30', '40', '50', '60'])
-            ax[1].set_xlabel('Time (min)')        
-        except: pass
-        try:
-            combined = np.concatenate((self.data, self.dataUV), axis=0)
-            upper = jmf.findpercentilevalue(combined, 0.95)
-            lower = jmf.findpercentilevalue(combined, 0.05)
-            ax[1].set_ylim([lower, upper])
-        except: print('Getting y-axis limits for session viewer is not working')
-        
-        try:
-            for i, x in enumerate(self.epochfields):
-                print(i, x)
-#                onset = getattr(output, x).onset * self.fs
-#                ax[0].scatter(onset, [i]*len(onset))
-        except:
-            print('could not plot events')
-            pass
-        
-        canvas = FigureCanvasTkAgg(f, self.f2)
-        canvas.draw()
-        canvas.get_tk_widget().grid(row=0, column=0, sticky=(N,S,E,W))
-        
+    def getnoiseindex(self):
+        self.noisemethod = 'sum'
+        self.threshold = 8
+        bgMAD = findnoise(self.data, self.randomevents,
+                              t2sMap = self.t2sMap, fs = self.fs, bins=self.bins,
+                              method=self.noisemethod)          
+        sigSum = [np.sum(abs(i)) for i in self.snips['blue']]
+        sigSD = [np.std(i) for i in self.snips['blue']]
+        self.noiseindex = [i > bgMAD*self.threshold for i in sigSum]
+
+    def makelickruns(self):
+        self.setevents()
+        # need to set lick runs as if a normal output variable
+        self.runs = [val for i, val in enumerate(self.licks) if (val - self.licks[i-1] > 10)]
+        self.epochfields.append('runs')
+        self.updateeventoptions()
+
     def singletrialviewer(self):
         f = Figure(figsize=(3,2)) # 5,3
         ax = f.add_subplot(111)
@@ -352,6 +369,145 @@ def makerandomevents(minTime, maxTime, spacing = 77, n=100):
         start = start + spacing
     events = [i+minTime for i in events]
     return events
+
+def snipper(data, timelock, fs = 1, t2sMap = [], preTrial=10, trialLength=30,
+                 adjustBaseline = True,
+                 bins = 0):
+
+    if len(timelock) == 0:
+        print('No events to analyse! Quitting function.')
+        raise Exception('no events')
+
+    pps = int(fs) # points per sample
+    pre = int(preTrial*pps) 
+#    preABS = preTrial
+    length = int(trialLength*pps)
+# converts events into sample numbers
+    event=[]
+    if len(t2sMap) > 1:
+        for x in timelock:
+            event.append(np.searchsorted(t2sMap, x, side="left"))
+    else:
+        event = [x*fs for x in timelock]
+
+    new_events = []
+    for x in event:
+        if int(x-pre) > 0:
+            new_events.append(x)
+    event = new_events
+
+    nSnips = len(event)
+    snips = np.empty([nSnips,length])
+    avgBaseline = []
+
+    for i, x in enumerate(event):
+        start = int(x) - pre
+        avgBaseline.append(np.mean(data[start : start + pre]))
+        try:
+            snips[i] = data[start : start+length]
+        except ValueError: # Deals with recording arrays that do not have a full final trial
+            snips = snips[:-1]
+            avgBaseline = avgBaseline[:-1]
+            nSnips = nSnips-1
+
+    if adjustBaseline == True:
+        snips = np.subtract(snips.transpose(), avgBaseline).transpose()
+        snips = np.divide(snips.transpose(), avgBaseline).transpose()
+
+    if bins > 0:
+        if length % bins != 0:
+            snips = snips[:,:-(length % bins)]
+        totaltime = snips.shape[1] / int(fs)
+        snips = np.mean(snips.reshape(nSnips,bins,-1), axis=2)
+        pps = bins/totaltime
+              
+    return snips, pps
+
+def mastersnipper(x, events,
+                  bins=300,
+                  preTrial=10,
+                  trialLength=30,
+                  threshold=10,
+                  peak_between_time=[0, 1],
+                  output_as_dict=True,
+                  latency_events=[],
+                  latency_direction='pre'):
+    if len(events) < 1:
+        print('Cannot find any events. All outputs will be empty.')
+        blueTrials, uvTrials, noiseindex, diffTrials, peak, latency = ([] for i in range(5))
+    else:
+        blueTrials,_ = snipper(x.data, events,
+                                   t2sMap=x.t2sMap,
+                                   fs=x.fs,
+                                   bins=bins,
+                                   preTrial=preTrial,
+                                   trialLength=trialLength)
+        uvTrials,_ = snipper(x.datauv, events,
+                                   t2sMap=x.t2sMap,
+                                   fs=x.fs,
+                                   bins=bins,
+                                   preTrial=preTrial,
+                                   trialLength=trialLength)
+        filtTrials,_ = snipper(x.datafilt, events,
+                                   t2sMap=x.t2sMap,
+                                   fs=x.fs,
+                                   bins=bins,
+                                   preTrial=preTrial,
+                                   trialLength=trialLength,
+                                   adjustBaseline = False)
+        
+        filtTrials_z = zscore(filtTrials)
+        
+        bgMAD = findnoise(x.data, x.randomevents,
+                              t2sMap=x.t2sMap, fs=x.fs, bins=bins,
+                              method='sum')        
+        sigSum = [np.sum(abs(i)) for i in blueTrials]
+        sigSD = [np.std(i) for i in blueTrials]
+        noiseindex = [i > bgMAD*threshold for i in sigSum]
+
+        output = {}
+        output['blue'] = blueTrials
+        output['uv'] = uvTrials
+        output['filt'] = filtTrials
+        output['filtz'] = filtTrials_z
+        output['noise'] = noiseindex
+        
+        return output
+
+def zscore(snips, baseline_points=100):
+    
+    BL_range = range(baseline_points)
+    z_snips = []
+    for i in snips:
+        mean = np.mean(i[BL_range])
+        sd = np.std(i[BL_range])
+        z_snips.append([(x-mean)/sd for x in i])
+        
+    return z_snips
+
+def findnoise(data, background, t2sMap = [], fs = 1, bins=0, method='sd'):
+    
+    bgSnips, _ = snipper(data, background, t2sMap=t2sMap, fs=fs, bins=bins)
+    
+    if method == 'sum':
+        bgSum = [np.sum(abs(i)) for i in bgSnips]
+        bgMAD = med_abs_dev(bgSum)
+    elif method == 'sd':
+        bgSD = [np.std(i) for i in bgSnips]
+        bgMAD = med_abs_dev(bgSD)
+   
+    return(bgMAD)
+
+def removenoise(snipsIn, noiseindex):
+    snipsOut = np.array([x for (x,v) in zip(snipsIn, noiseindex) if not v])   
+    return snipsOut
+
+def med_abs_dev(data, b=1.4826):
+    median = np.median(data)
+    devs = [abs(i-median) for i in data]
+    mad = np.median(devs)*b
+                   
+    return mad
 
 root = Tk()
 
--- temp.py
+++ temp.py
@@ -1,51 +1,8 @@
-import scipy.io as sio
-import matplotlib.pyplot as plt
-import JM_general_functions as jmf
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 22 07:41:30 2019
 
-
-def scale_axis(signal):
-    upper = jmf.findpercentilevalue(signal, 0.95)
-    lower = jmf.findpercentilevalue(signal, 0.05)
-    return [upper, lower]
-
-filename = 'R:\\DA_and_Reward\\es334\\MCP1\\matfiles\\MCP1-1_s1.mat'
-
-
-a = sio.loadmat(filename, squeeze_me=True, struct_as_record=False)
-
-output = a['output']
-
-blue = output.blue
-uv = output.uv
-
-TTLs = []
-for x in output._fieldnames:
-    var = getattr(output, x)
-    if hasattr(var, 'onset'):
-        TTLs.append(x)
-
-
-try:
-    fs = output.fs
-except:
-    fs = output.fs1 
-
-
-
-f, ax = plt.subplots(nrows=3, ncols=1, sharex=True)
-
-# Work out TTLs
-for i,x in enumerate(TTLs):
-    onset = getattr(output, TTLs[0]).onset * fs
-    ax[0].scatter(onset, [i]*len(onset))
-
-
-# Plot blue signal
-ax[1].plot(blue, color='blue')
-ax[1].set_ylim(scale_axis(blue))
-
-# Plot UV signal
-ax[2].plot(uv, color='magenta')
-ax[2].set_ylim(scale_axis(uv))
-
-
+@author: jmc010
+"""
+tdtfile='C:\\Github\\PPP_analysis\\data\\Eelke-171027-111329\\'
+tmp = tdt.read_block(tdtfile, evtype=['epocs'])
